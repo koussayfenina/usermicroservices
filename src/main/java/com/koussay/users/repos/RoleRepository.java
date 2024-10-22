7ecde85ae62164a7e82216d2295f48cd64d67278
koussayfenina <146864045+koussayfenina@users.noreply.github.com>
2024-10-22 20:05:55 +0100

    microservices

---
+++ src/main/java/com/koussay/users/repos/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.koussay.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.koussay.users.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+	Role findByRole(String role);
+	
+}
---
+++ src/main/java/com/koussay/users/repos/UserRepository.java
@@ -0,0 +1,11 @@
+package com.koussay.users.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.koussay.users.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+		User findByUsername(String username);
+
+}
